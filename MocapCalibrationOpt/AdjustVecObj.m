3f270b570b2d2d841a2e18de94425ffeb052339a
Jiaji <jiajiz@cs.cmu.edu>
2015-03-29 13:34:33 -0400

    add Matlab Opt

---
+++ MocapCalibrationOpt/AdjustVecObj.m
@@ -0,0 +1,10 @@
+% This function adjusts the object vector (manual measurement initialization)
+% represented in the robot tool frame
+% Input:
+% Q: A set of mocap points transformed back to the robot frame
+% 
+function [Pobj] = AdjustVecObj()
+
+
+end
+
