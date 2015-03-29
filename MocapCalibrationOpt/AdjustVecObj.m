0f5c4a2fa7f6de035c8889e5c9bfadac283848ac
Jiaji <jiajiz@cs.cmu.edu>
2015-03-29 15:46:38 -0400

    add Matlab Opt

--- MocapCalibrationOpt/AdjustVecObj.m
+++ MocapCalibrationOpt/AdjustVecObj.m
@@ -1,10 +1,13 @@
 % This function adjusts the object vector (manual measurement initialization)
 % represented in the robot tool frame
 % Input:
-% Q: A set of mocap points transformed back to the robot frame
-% 
-function [Pobj] = AdjustVecObj()
-
-
+% P: Set of points(tool center) in robot base frame.
+% Q: Corresponding set of mocap points transformed back to the base frame
+% Output:
+% v: The refitted vector that corresponds to the marker point shifting
+% vector in base frame. We are assuming the tool phalange is always pointing downwards.
+% Hence, the rotation matrix that represents tool frame in base frame is fixed. 
+function [v] = AdjustVecObj(P, Q)
+v = mean(Q - P);
 end
 
---
+++ MocapCalibrationOpt/demo_cali.m
@@ -0,0 +1,2 @@
+% Give initialization from physical measurement.
+v = [0; 0; -133.68];
